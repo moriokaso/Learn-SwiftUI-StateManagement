c5d2fcc07c9db92efdc46f3665164af2151c8a77
so morioka <morioka.so@moneyforward.co.jp>
2025-05-26 17:08:17 +0900

    Initial Commit

---
+++ LearnStateManagement/LearnStateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnStateManagementApp.swift
+//  LearnStateManagement
+//
+//  Created by morioka.so on 2025/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnStateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
